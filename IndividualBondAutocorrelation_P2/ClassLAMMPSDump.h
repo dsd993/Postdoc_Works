dfcb24d5d31494eaf1ffbd71de910cb362b7eb80
dsd993 <90120674+dsd993@users.noreply.github.com>
2021-09-20 21:18:15 -0500

    Add files via upload

---
+++ IndividualBondAutocorrelation_P2/ClassLAMMPSDump.h
@@ -0,0 +1,84 @@
+//This header file reads dump files at different time steps
+
+#include<fstream>
+#include<iostream>
+#include<cstdio>
+#include<string>
+#include<cstdlib>
+#include<iomanip>
+#include<limits>
+#include<math.h>
+#include<sstream>
+
+using namespace std;
+
+class Coord
+{
+public:
+	double x, y, z;
+	int ix, iy, iz;
+};
+
+class DumpCoord
+{
+public:
+	int AtomID, MoleculeID, AtomTypeID;
+	float Charge, Mass;
+	Coord AtomPosition;
+	Coord AtomImageFlags;
+
+	void ReadCoord(ifstream &in)
+	{
+		in >> AtomID >> MoleculeID >> AtomTypeID >> Charge >> Mass >> AtomPosition.x >> AtomPosition.y >> AtomPosition.z >> AtomImageFlags.ix >> AtomImageFlags.iy >> AtomImageFlags.iz;
+	}
+};
+
+//Class to read the dump files
+class DumpFileData
+{
+public:
+	int TimeStep, NumberofAtoms;
+	Coord BoxMin, BoxMax;
+	DumpCoord *AtomsCoord;
+
+	void ReadfromFile(string Filename)
+	{
+		ifstream in(Filename.c_str());
+		if (!in)
+		{
+			cout << "Error reading " << Filename;
+			cin >> Filename;
+		}
+
+		string tempstring;
+		in >> tempstring >> tempstring;
+		in >> TimeStep;
+		in >> tempstring >> tempstring >> tempstring >> tempstring;
+		in >> NumberofAtoms;
+		in >> tempstring >> tempstring >> tempstring >> tempstring >> tempstring >> tempstring;
+		in >> BoxMin.x >> BoxMax.x >> BoxMin.y >> BoxMax.y >> BoxMin.z >> BoxMax.z;
+
+		in >> tempstring >> tempstring >> tempstring >> tempstring >> tempstring >> tempstring >> tempstring >> tempstring >> tempstring >> tempstring >> tempstring >> tempstring >> tempstring;
+		AtomsCoord = new DumpCoord[NumberofAtoms];
+		for (int AtomsCounter = 0; AtomsCounter<NumberofAtoms; ++AtomsCounter)
+		{
+			AtomsCoord[AtomsCounter].ReadCoord(in);
+		}
+			
+		in.close();
+	}
+
+	void DestroyDumpFileData()
+	{
+		delete[] AtomsCoord; AtomsCoord = NULL;
+	}
+};
+
+string FilenameDotTimestep(string InitialFilename, int TimeStep)
+{
+	/* Convert Timestep to append to filename */
+	stringstream TimeStream;
+	TimeStream << TimeStep;
+	return(InitialFilename + '.' + TimeStream.str());
+}
+
