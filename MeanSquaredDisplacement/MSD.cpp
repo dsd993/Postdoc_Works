67a7b146ce7d4eb1a4c316f9160832dad15a035e
dsd993 <90120674+dsd993@users.noreply.github.com>
2021-10-12 00:30:11 -0500

    Update MSD.cpp

--- MeanSquaredDisplacement/MSD.cpp
+++ MeanSquaredDisplacement/MSD.cpp
@@ -1,4 +1,4 @@
-//Written by Dinesh S. Devarajan, dated 09/24/2021
+//Written by Dinesh S. Devarajan, dated 10/12/2021
 
 //Code to calculate the averaged mean squared displacement (MSD) of the individual beads in chains and the COM MSD of the chains
 
